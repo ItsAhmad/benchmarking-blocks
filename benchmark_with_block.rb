ab48e03f69d45131e38ffb4a5abe815ab5d04fc1
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-07-24 16:47:07 -0400

    implement block for benchmark

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,8 @@
 def benchmark
-  # Your benchmarking code goes here.
+  time = Benchmark.measure do 
+    yield
+  end 
+  time 
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
